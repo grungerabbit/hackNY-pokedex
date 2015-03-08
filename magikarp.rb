eeb6a9f14dba74c2315c76b63343da52d1a41430
Cheryl Wu <grungerabbit@gmail.com>
2015-03-08 11:45:27 -0400

    2014 and all fellows json

--- magikarp.rb
+++ magikarp.rb
@@ -3,22 +3,25 @@ require 'open-uri'
 require 'pry'
 require 'json'
 require 'awesome_print'
-# require 'sinatra'
+require 'sinatra'
 
 response_2010 = open 'http://hackny.org/a/2010/06/announcing-the-2010-hackny-fellows/'
 response_2011 = open 'http://hackny.org/a/2011/06/class-of-2011-hackny-fellows/'
 response_2012 = open 'http://hackny.org/a/2012/06/class-of-2012-hackny-fellows/'
 response_2013 = open 'http://hackny.org/a/2013/06/hackny-announces-the-class-of-2013-hackny-fellows/'
+response_2014 = open 'http://hackny.org/a/2014/07/hackny-2014-fellowship-demofest-and-class-announcement/'
 
 class2010_document = Nokogiri::HTML response_2010 
 class2011_document = Nokogiri::HTML response_2011
 class2012_document = Nokogiri::HTML response_2012
 class2013_document = Nokogiri::HTML response_2013
+class2014_document = Nokogiri::HTML response_2014
 
 fellow_rows_2010 = class2010_document.css 'tr'
 fellow_rows_2011 = class2011_document.css 'tr'
 fellow_rows_2012 = class2012_document.css 'tr'
 fellow_rows_2013 = class2013_document.css 'tr'
+fellow_rows_2014 = class2014_document.css 'tr'
 
 poke_number = 0 
 
@@ -96,6 +99,26 @@ fellow_hashes_2013 = fellow_rows_2013.map do |fellow_row|
 	student_name = student_td.name != "th" ? student_td.children.children[1].text : ""
 	university_name = university_td.name != "th" ? university_td.children.text : ""
 
+	# remove newline
+	student_name[0] = ""
+
+	{
+		# company: company_name,
+		name: student_name,
+		university: university_name
+	}
+end
+
+# 2014!!
+fellow_hashes_2014 = fellow_rows_2014.map do |fellow_row|
+	row_data = fellow_row.children.select do |child|
+		child.text != "\n"
+	end
+
+	student_td, university_td = row_data
+	student_name = student_td.children.text
+	university_name = university_td.children.text
+
 	{
 		# company: company_name,
 		name: student_name,
@@ -104,6 +127,7 @@ fellow_hashes_2013 = fellow_rows_2013.map do |fellow_row|
 end
 
 fellow_hashes_2013.slice!(0); # delete empty header
+fellow_hashes_2014.slice!(0); # delete empty header
 
 fellow_2010 = fellow_hashes_2010.sort_by do |fellow|
 	fellow[:name]
@@ -117,9 +141,12 @@ end
 fellow_2013 = fellow_hashes_2013.sort_by do |fellow|
 	fellow[:name]
 end
+fellow_2014 = fellow_hashes_2014.sort_by do |fellow|
+	fellow[:name]
+end
 
-# get '/2010.json' do
-#	fellow_hashes.to_json
-# end
+all_fellows = fellow_2010.concat(fellow_2011).concat(fellow_2012).concat(fellow_2013).concat(fellow_2014)
 
-binding.pry 
+get '/fellows.json' do
+	all_fellows.to_json
+end
