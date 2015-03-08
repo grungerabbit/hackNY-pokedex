e7dea0143acc4a93f2746d9917f1aa2350cfc172
Cheryl Wu <grungerabbit@gmail.com>
2015-03-08 11:22:02 -0400

    add 2013

--- magikarp.rb
+++ magikarp.rb
@@ -8,14 +8,17 @@ require 'awesome_print'
 response_2010 = open 'http://hackny.org/a/2010/06/announcing-the-2010-hackny-fellows/'
 response_2011 = open 'http://hackny.org/a/2011/06/class-of-2011-hackny-fellows/'
 response_2012 = open 'http://hackny.org/a/2012/06/class-of-2012-hackny-fellows/'
+response_2013 = open 'http://hackny.org/a/2013/06/hackny-announces-the-class-of-2013-hackny-fellows/'
 
 class2010_document = Nokogiri::HTML response_2010 
 class2011_document = Nokogiri::HTML response_2011
 class2012_document = Nokogiri::HTML response_2012
+class2013_document = Nokogiri::HTML response_2013
 
 fellow_rows_2010 = class2010_document.css 'tr'
 fellow_rows_2011 = class2011_document.css 'tr'
 fellow_rows_2012 = class2012_document.css 'tr'
+fellow_rows_2013 = class2013_document.css 'tr'
 
 poke_number = 0 
 
@@ -81,7 +84,26 @@ fellow_hashes_2012 = fellow_rows_2012.map do |fellow_row|
 end
 
 
+# 2013!!
+fellow_hashes_2013 = fellow_rows_2013.map do |fellow_row|
+	row_data = fellow_row.children.select do |child|
+		child.text != "\n"
+		# defined?(child.name) ? child.name != "th" : child.text != "\n"
+	end
 
+	student_td, university_td = row_data
+
+	student_name = student_td.name != "th" ? student_td.children.children[1].text : ""
+	university_name = university_td.name != "th" ? university_td.children.text : ""
+
+	{
+		# company: company_name,
+		name: student_name,
+		university: university_name
+	}
+end
+
+fellow_hashes_2013.slice!(0); # delete empty header
 
 fellow_2010 = fellow_hashes_2010.sort_by do |fellow|
 	fellow[:name]
@@ -90,6 +112,9 @@ fellow_2011 = fellow_hashes_2011.sort_by do |fellow|
 	fellow[:name]
 end
 fellow_2012 = fellow_hashes_2012.sort_by do |fellow|
+	fellow[:name]
+end
+fellow_2013 = fellow_hashes_2013.sort_by do |fellow|
 	fellow[:name]
 end
 
