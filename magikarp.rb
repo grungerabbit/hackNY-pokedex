d34a6aac4838a4346919bec40ec81560108be1b2
Cheryl Wu <grungerabbit@gmail.com>
2015-03-08 09:03:08 -0400

    start magikarp.rb

---
+++ magikarp.rb
@@ -0,0 +1,41 @@
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+require 'json'
+require 'awesome_print'
+
+response = open 'http://hackny.org/a/2010/06/announcing-the-2010-hackny-fellows/'
+
+class2010_document = Nokogiri::HTML response 
+
+fellow_rows = class2010_document.css 'tr'
+
+poke_number = 0 
+
+fellow_hashes = fellow_rows.map do |fellow_row|
+
+
+	row_data = fellow_row.children.select do |child|
+		child.text != "\n" 
+	end 
+
+	logo_img, company_td, student_td, university_td = row_data 
+
+
+	binding.pry
+
+	company_name = company_td.children.children.text
+
+	university_name = university_td.children.text
+
+	poke_number += 1 
+
+	{
+		company: company_name,
+		name: student_name,
+		university: university_name
+	}
+
+end 
+
+binding.pry 
