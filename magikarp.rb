64109c18ed89efcf0e773411a94ddd6e55f15d3f
Cheryl Wu <grungerabbit@gmail.com>
2015-03-08 10:16:15 -0400

    add 2011

--- magikarp.rb
+++ magikarp.rb
@@ -3,19 +3,22 @@ require 'open-uri'
 require 'pry'
 require 'json'
 require 'awesome_print'
-require 'sinatra'
+# require 'sinatra'
 
-response = open 'http://hackny.org/a/2010/06/announcing-the-2010-hackny-fellows/'
+response_2010 = open 'http://hackny.org/a/2010/06/announcing-the-2010-hackny-fellows/'
+response_2011 = open 'http://hackny.org/a/2011/06/class-of-2011-hackny-fellows/'
 
-class2010_document = Nokogiri::HTML response 
 
-fellow_rows = class2010_document.css 'tr'
+class2010_document = Nokogiri::HTML response_2010 
+class2011_document = Nokogiri::HTML response_2011
+
+fellow_rows_2010 = class2010_document.css 'tr'
+fellow_rows_2011 = class2011_document.css 'tr'
 
 poke_number = 0 
 
-fellow_hashes = fellow_rows.map do |fellow_row|
-
-
+# 2010!!
+fellow_hashes_2010 = fellow_rows_2010.map do |fellow_row|
 	row_data = fellow_row.children.select do |child|
 		child.text != "\n" 
 	end 
@@ -34,10 +37,43 @@ fellow_hashes = fellow_rows.map do |fellow_row|
 		name: student_name,
 		university: university_name
 	}
-end 
-
-get '/2010.json' do
-	fellow_hashes.to_json
 end
 
-# binding.pry 
+# 2011!!
+fellow_hashes_2011 = fellow_rows_2011.map do |fellow_row|
+	row_data = fellow_row.children.select do |child|
+		child.text != "\n" 
+	end 
+
+	student_td, university_td, company_td= row_data 
+
+	student_name = student_td.children.text
+	university_name = university_td.children.text
+	company_name = company_td.children.children[1].attributes["alt"].value
+
+	poke_number += 1 
+
+	{
+		company: company_name,
+		name: student_name,
+		university: university_name
+	}
+	# binding.pry
+end
+
+
+
+
+
+fellow_2010 = fellow_hashes_2010.sort_by do |fellow|
+	fellow[:name]
+end
+fellow_2011 = fellow_hashes_2011.sort_by do |fellow|
+	fellow[:name]
+end
+
+# get '/2010.json' do
+#	fellow_hashes.to_json
+# end
+
+binding.pry 
